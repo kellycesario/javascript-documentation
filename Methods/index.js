ae209e5e4ab9c8496b6dfa845ee32e7da8f2c894
kellycesario <kellycesario@gmail.com>
2022-10-27 08:50:52 -0300

    Add new file about .slice, .split, .startsWith

---
+++ Methods/index.js
@@ -0,0 +1,12 @@
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const getCode = str => str.slice(0, 3).toUpperCase()
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`
+
+  console.log(output)
+}
